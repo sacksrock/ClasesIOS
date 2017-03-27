cd73629fbf8f9aa210eddb9f237008b990266f5a
daniel de la iglesia <gradomedio2smr@gmail.com>
2017-03-27 13:06:09 +0200

    Coleccion

---
+++ Actividad1/TablaColeccionCell.swift
@@ -0,0 +1,13 @@
+//
+//  TablaColeccionCell.swift
+//  Actividad1
+//
+//  Created by Daniel de la Iglesia Gonzalez on 27/3/17.
+//  Copyright © 2017 Daniel de la Iglesia Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class TablaColeccionCell: UICollectionViewCell {
+    
+}
